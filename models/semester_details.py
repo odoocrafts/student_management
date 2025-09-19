466c2c4b177afa67637eef7b378c911068098573
odoo <muhsink00000@gmail.com>
2025-09-19 17:51:14 +0530

    added domain for semester details

--- models/semester_details.py
+++ models/semester_details.py
@@ -7,7 +7,6 @@ class SemesterFeeDetails(models.Model):
     name = fields.Char(string="Name", required=True)
     student_id = fields.Many2one('student.student', string="Student", ondelete="cascade")
     semester = fields.Char(string="Semester")
-
     fee_amount = fields.Float(string="Fee Amount", required=True)
     paid_amount = fields.Float(string="Paid Amount", compute='_compute_allocate_paid_amount', store=True)
     balance_amount = fields.Float(string="Balance Amount", compute="_compute_balance", store=True)
@@ -28,20 +27,21 @@ class SemesterFeeDetails(models.Model):
     @api.depends('student_id.paid_amount')
     def _compute_allocate_paid_amount(self):
         for student in self:
-            remaining_payment = student.student_id.paid_amount
+            if student.student_id.course_type == 'degree':
+                remaining_payment = student.student_id.paid_amount
 
-            for sem in student.student_id.semester_records_ids:
-                if remaining_payment <= 0:
-                    break
+                for sem in student.student_id.semester_records_ids:
+                    if remaining_payment <= 0:
+                        break
 
-                # full or partial payment for this semester
-                if remaining_payment >= sem.fee_amount:
-                    sem.paid_amount = sem.fee_amount
-                    sem.balance_amount = 0.0
-                    sem.status = 'paid'  # assuming selection: draft/paid/partial
-                    remaining_payment -= sem.fee_amount
-                else:
-                    sem.paid_amount = remaining_payment
-                    sem.balance_amount = sem.fee_amount - remaining_payment
-                    sem.status = 'partial'
-                    remaining_payment = 0
+                    # full or partial payment for this semester
+                    if remaining_payment >= sem.fee_amount:
+                        sem.paid_amount = sem.fee_amount
+                        sem.balance_amount = 0.0
+                        sem.status = 'paid'  # assuming selection: draft/paid/partial
+                        remaining_payment -= sem.fee_amount
+                    else:
+                        sem.paid_amount = remaining_payment
+                        sem.balance_amount = sem.fee_amount - remaining_payment
+                        sem.status = 'partial'
+                        remaining_payment = 0
